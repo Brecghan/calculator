9734439ec58c5c69ec7bb193501ef49fdf59f2de
Brecghan Ryan <Brecghan.Ryan@Gmail.com>
2023-03-31 15:01:24 -0500

    added bonus stuff

--- App.java
+++ App.java
@@ -1,54 +1,90 @@
 import java.util.Scanner;
+import java.util.Set;
+
 public class App {
     static final Scanner inputScanner = new Scanner(System.in);
-   
-    public static void main (String[] args) {
+    private static final Set<String> yesValues = Set.of("yes", "y");
+    private static final Set<String> noValues = Set.of("no", "n");
+    private static final Set<String> addValues = Set.of("add", "+", "addition", "a");
+    private static final Set<String> subValues = Set.of("subtract", "sub", "subtraction", "s");
+    private static final Set<String> multValues = Set.of("multiply", "*", "x", "multiplication", "m");
+    private static final Set<String> diviValues = Set.of("divide", "/", "division", "d");
+
+    public static void main(String[] args) {
         boolean checkCont = true;
-        while(checkCont){
-        String thisFun = getString("Enter the numbers you wish to math seperated by a space");
-        // double num1 = getDouble("Enter first number");
-        // double num2 = getDouble("Enter second number");
+        Boolean reUse = false;
+        Double totalAns = 0.0;
         
-        double num1 = 
-        String whatDo = getString("Add, Subtract, Multiply, or Divide?");
-        Calculator ans = new Calculator();
-        double addAns = ans.add(num1,num2);
-        double subAns = ans.subtract(num1,num2);
-        double multAns = ans.multiply(num1,num2);
-        double diviAns = ans.divide(num1,num2);
-        if (whatDo.equals("Add")){
-            System.out.println(String.format("%f plus %f equals %f",num1,num2,addAns));
-        }
-        if (whatDo.equals("Subtract")){
-            System.out.println(String.format("%f minus %f equals %f",num1,num2,subAns));
-        }
-        if (whatDo.equals("Multiply")){
-            System.out.println(String.format("%f times %f equals %f",num1,num2,multAns));
-        }
-        if (whatDo.equals("Divide")){
-            if (Double.isInfinite(diviAns)){
-                System.out.println("ONLY GOD CAN DIVIDE BY ZERO!! HEATHEN!!!");
+        while (checkCont) {
+            Double num1 = 0.0;
+            Double num2 = 0.0;
+            String oper = "";
+            if (reUse == false) {
+                String thisFun = getInput("Enter your equation in this format - Number*SPACE*Operation*SPACE*Number");
+                num1 = inputScanner.nextDouble();
+                oper = inputScanner.next();
+                num2 = inputScanner.nextDouble();
+            } else {
+                oper = getString("Which operation would you like to use?");
+                num2 = getDouble("With what number?");
+                num1 = totalAns;
+            }
+            System.out.println("Num 1 is " + num1);
+            System.out.println("Oper is " + oper);
+            System.out.println("Num 2 is " + num2);
+            Calculator ans = new Calculator();
+            double addAns = ans.add(num1, num2);
+            double subAns = ans.subtract(num1, num2);
+            double multAns = ans.multiply(num1, num2);
+            double diviAns = ans.divide(num1, num2);
+            if (addValues.contains(oper)){
+                System.out.println(String.format("%f plus %f equals %f", num1, num2, addAns));
+                totalAns = addAns;
+            }
+            if (subValues.contains(oper)) {
+                System.out.println(String.format("%f minus %f equals %f", num1, num2, subAns));
+                totalAns = subAns;
+            }
+            if (multValues.contains(oper)) {
+                System.out.println(String.format("%f times %f equals %f", num1, num2, multAns));
+                totalAns = multAns;
+            }
+            if (diviValues.contains(oper)) {
+                if (Double.isInfinite(diviAns)) {
+                    System.out.println("ONLY GOD CAN DIVIDE BY ZERO!! HEATHEN!!!");
+                    break;
+                } else {
+                    System.out.println(String.format("%f divide by %f equals %f", num1, num2, diviAns));
+                    totalAns = diviAns;
+                }
+            }
+            String cont = getString("Do you want to Continue Yes or No?");
+            if (noValues.contains(cont.toLowerCase())) {
+                System.out.println("Goodbye");
                 break;
             }
-            else {
-            System.out.println(String.format("%f divide by %f equals %f",num1,num2,diviAns));
+            String reMath = getString("Do you want use this answer in the next equation?");
+            if (yesValues.contains(reMath.toLowerCase())) {
+                reUse = true;
+            } else {
+                reUse = false;
             }
         }
-        //System.out.println(String.format("You want to math with %f and %f - addition is %f, subtraction is %f, multiplication is %f, and division is %f", num1, num2, addAns, subAns, multAns, diviAns));
-        String cont = getString("Do you want to Continue Yes or No?");
-        if (!cont.equals("Yes")){
-            System.out.println("Goodbye");
-            break;}
-        }
     }
-    
 
-    private static String getString(String prompt){
+    private static String getString(String prompt) {
         System.out.println(prompt);
         return inputScanner.next();
     }
-    private static double getDouble(String prompt){
+
+    private static double getDouble(String prompt) {
         System.out.println(prompt);
         return inputScanner.nextDouble();
+    }
+
+    private static String getInput(String prompt) {
+        System.out.println(prompt);
+        String ans = "";
+        return ans;
     }
 }
